848e192d8e611483a47f02d1d9e4d94d8bbfc474
Jens Lundeqvist <jelu000@gmail.com>
2021-02-23 21:04:19 +0100

    Klar med modules sid 100

--- print.js
+++ print.js
@@ -1,7 +1,19 @@
+import { calcMoms } from "./tax2";
+
+
 export function print1() {
   console.log("print1");
 }
 
 export function print2() {
-  console.log("print2");  
+  console.log("print2");
+}
+
+export function printDetails(product){
+  let taxedPrice = calcMoms(product.price);
+  console.log(`Namn: ${product.name}, pris med moms: ${taxedPrice.toFixed(2)}:-`);
+}
+
+export function draRabbat(product, rabbat = 10){
+ product.price = product.price - rabbat;
 }
--- tax.js
+++ tax.js
@@ -1,5 +1,5 @@
 //använder default exports nä endast ett värde ska exporteras från en modul
 export default function(price) {
-  console.log("Beräknar moms! - default exports när endast ett värde ska exporteras från en modul");
-  return Number(price) * 1.25;
+  console.log("Beräknar Engelsk tax! - default exports när endast ett värde ska exporteras från en modul");
+  return Number(price) * 1.20;
 }
